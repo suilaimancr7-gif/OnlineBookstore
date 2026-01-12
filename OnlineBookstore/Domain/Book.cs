6c98f30b907c0e0d98e1d374b47e0076966faeca
Muhammad Zayd <suilaimancr7@gmail.com>
2026-01-12 15:45:44 +0800

    updates

--- OnlineBookstore/Domain/Book.cs
+++ OnlineBookstore/Domain/Book.cs
@@ -3,6 +3,7 @@
     public class Book : BaseDomainModel
     {
         public string? Title { get; set; }
+        public string? BookID { get; set; }
         public string? Author { get; set; }
         public string? Genre { get; set; }
         public string? ISBM { get; set; }
--- OnlineBookstore/Domain/Customer.cs
+++ OnlineBookstore/Domain/Customer.cs
@@ -2,8 +2,13 @@
 {
     public class Customer : BaseDomainModel
     {
+        public string? CustID { get; set; }
         public string? Name { get; set; }
         public string? Email { get; set; }
+        public string? Password { get; set; }
+        public int? PhoneNumber { get; set; }
+        public string? Address { get; set; }
+
     }
 }
 
