02464f5cc2b16467ad82c5ee1cac45696cc247af
Muhammad Zayd <suilaimancr7@gmail.com>
2026-01-14 11:28:06 +0800

    changes

--- OnlineBookstore/Domain/Order.cs
+++ OnlineBookstore/Domain/Order.cs
@@ -5,7 +5,7 @@ namespace OnlineBookstore.Domain
     public class Order : BaseDomainModel
     {
         public int CustomerId { get; set; }
-        public Customer? Customer { get; set; }
+        public Customer? CustomerID { get; set; }
         public ICollection<OrderDetail>? OrderDetails { get; set; }
     }
 }
